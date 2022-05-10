cafcb22233cc7224f85b5642f1326f3eac1ccdde
felix <gicubezereu1997@gmail.com>
2022-05-10 22:59:26 +0300

    sdfsdf

--- news/models.py
+++ news/models.py
@@ -4,17 +4,17 @@ from google_trends.models import Trend
 class Post(models.Model):
     trend = models.ForeignKey(Trend, on_delete=models.CASCADE, null=True)
 
-    uuid = models.CharField(max_length=250)
-    title = models.CharField(max_length=250, null=True, blank=True)
+    uuid = models.CharField(max_length=2083)
+    title = models.CharField(max_length=2083, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    keywords = models.CharField(max_length=250, null=True, blank=True)
-    snippet = models.CharField(max_length=250, null=True, blank=True)
-    url = models.CharField(max_length=250, null=True, blank=True)
+    keywords = models.CharField(max_length=2083, null=True, blank=True)
+    snippet = models.CharField(max_length=2083, null=True, blank=True)
+    url = models.CharField(max_length=2083, null=True, blank=True)
     image_url = models.CharField(max_length=2083, null=True, blank=True)
-    language = models.CharField(max_length=250, null=True, blank=True)
+    language = models.CharField(max_length=2083, null=True, blank=True)
     published_at = models.DateTimeField(null=True, blank=True)
-    source = models.CharField(max_length=250, null=True, blank=True)
-    types = models.CharField(max_length=250, null=True, blank=True)
+    source = models.CharField(max_length=2083, null=True, blank=True)
+    types = models.CharField(max_length=2083, null=True, blank=True)
 # categories = ["general"]
 # relevance_score = None
     created_on = models.DateTimeField(auto_now_add=True)
